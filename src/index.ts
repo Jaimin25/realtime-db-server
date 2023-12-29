ff4b968942e64d44d8a33c0aca5b5cc65c5b1dfa
jaimin25 <jaiminchovatia25123@gmail.com>
2023-12-29 12:28:29 +0530

    initial

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { Server } from "socket.io";
+import express from "express";
+import http from "http";
+
+const app = express();
+const server = app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
+
+const socketIo = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+socketIo.on("connection", (socket) => {
+    socket.on("send-message", (message) => {
+        socketIo.emit("receive-message", message);
+    });
+});
